441c354261a4b86bf8accde8093f433c8cc0451a
Atul Manjhi <163382635+atulmanjhi1@users.noreply.github.com>
2024-04-27 13:32:49 +0530

    Add files via upload

---
+++ Ocean.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+import PIL.Image
+
+img = PIL.Image.open('e:\my documents\my document\WhatsApp Image 2024-04-24 at 20.14.27_eca7a31d.jpg')
+
+genai.configure(api_key='AIzaSyDzeLERYL7HHJwJvSofAUjVT1H3YUojd_8')
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+response = model.generate_content(["which object are there in this image", img])
+
+print(response.text)
