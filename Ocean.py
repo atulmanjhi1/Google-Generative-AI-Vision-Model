c849eb3495ec274f9199a2fc3824d3fceaeca17a
Atul Manjhi <163382635+atulmanjhi1@users.noreply.github.com>
2024-05-05 23:41:05 +0530

    Update Ocean.py

--- Ocean.py
+++ Ocean.py
@@ -3,7 +3,7 @@ import PIL.Image
 
 img = PIL.Image.open('e:\my documents\my document\WhatsApp Image 2024-04-24 at 20.14.27_eca7a31d.jpg')
 
-genai.configure(api_key='')
+genai.configure(api_key='Your_API_Key')
 
 model = genai.GenerativeModel('gemini-pro-vision')
 
